3be12ee6e4fcbdd2b1f2fbf133756aaad2a13a10
Karl Cridland <karlcridland@gmail.com>
2025-10-28 17:27:18 +0000

    Auth page started.

--- Alphabet of Dates/Features/Authentication/AuthenticationView.swift
+++ Alphabet of Dates/Features/Authentication/AuthenticationView.swift
@@ -6,10 +6,24 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct AuthenticationView: View {
     var body: some View {
-        Text("Authentication")
+        VStack {
+            
+        }
+        .toolbar {
+            ToolbarItem(placement: .bottomBar) {
+                Text("Sign In")
+                #warning("TODO: Sign In button here")
+            }
+        }
     }
 }
 
+#Preview {
+    NavigationStack {
+        AuthenticationView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
+}
