33996f31bce14964062ce3e02490abe894ebf9ea
Karl Cridland <karlcridland@gmail.com>
2025-12-29 22:30:34 +0000

    Activity comments view added.

---
+++ Alphabet of Dates/Features/Activities/Activity/Components/ActivityCommentsView.swift
@@ -0,0 +1,16 @@
+//
+//  ActivityCommentsView.swift
+//  Alphabet of Dates
+//
+//  Created by Karl Cridland on 29/12/2025.
+//
+
+import SwiftUI
+
+struct ActivityCommentsView: View {
+    
+    var body: some View {
+        Text("ActivityCommentsView")
+    }
+    
+}
